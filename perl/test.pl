d958be7741c0512c9f38340711505688eafa37a0
Toby Corkindale <tjc@wintrmute.net>
2012-05-18 12:12:24 +1000

    Allow pin to be specified via command line

--- perl/test.pl
+++ perl/test.pl
@@ -3,9 +3,11 @@ use 5.14.1;
 use warnings;
 use BeagleBone::Pins;
 
-say "This simply blinks power through the P9_11 pin at 1 Hz";
+my $id = shift || die "Please specify a pin name (eg. P9_11)\n";
 
-my $pin = BeagleBone::Pins->new('P9_11');
+say "This simply blinks power through the given pin at 1 Hz";
+
+my $pin = BeagleBone::Pins->new($id);
 say "Mux: " . $pin->mux;
 say "GPIO: " . $pin->gpio;
 
