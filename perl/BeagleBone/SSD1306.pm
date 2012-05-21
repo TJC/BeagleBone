7123ce9105500df1874e69cf883daaf92cde2272
Toby Corkindale <tjc@wintrmute.net>
2012-05-21 19:18:54 +1000

    POD documentation

--- perl/BeagleBone/SSD1306.pm
+++ perl/BeagleBone/SSD1306.pm
@@ -5,34 +5,72 @@ use BeagleBone::Pins;
 use BeagleBone::SPI;
 use Time::HiRes qw(usleep);
 use Carp qw(croak);
+use parent qw(Class::Accessor::Fast);
+__PACKAGE__->mk_accessors(qw(dc_pin rst_pin));
 
-# This is meant to be a driver for the SSD1306 LCD controller.
-# See http://www.adafruit.com/datasheets/SSD1306.pdf
+=head1 NAME
 
-# TODO: Allow these to be defined via parameters:
-#my $data = BeagleBone::Pins->new('P9_11');
-#my $clk = BeagleBone::Pins->new('P9_13');
-my $dc = BeagleBone::Pins->new('P9_15');
-my $rst = BeagleBone::Pins->new('P9_17');
-#my $cs = BeagleBone::Pins->new('P9_19');
+BeagleBone::SSD1306
+
+=head1 SYNOPSIS
+
+This is a driver for the SSD1306 LCD controller, for use from the BeagleBone.
+That said, it would probably work on other similar systems.
+
+See http://www.adafruit.com/datasheets/SSD1306.pdf for the LCD controller
+datasheet.
+
+Note that this driver requires the LCD controller's SPI pins (clk, cs, data) to
+be hooked up to the spi1 pins on the beaglebone, and for you to be using a
+distro with the spidev patches applied to the kernel.
+
+The ubuntu 12.04 distro seems to have them already applied. Angstrom probably
+does too -- check for existence of /dev/spidev2.0
+
+=head1 METHODS
+
+=cut
+
+=head2 new
+
+Constructor.
+
+Takes arguments for dc_pin and rst_pin.
+
+  my $lcd = BeagleBone::SSD1306->new(
+    dc_pin => 'P9_15',
+    rst_pin => 'P9_17',
+  );
+
+=cut
 
 sub new {
     my ($class, %args) = @_;
     my $self = {};
     bless $self, $class;
+    $self->dc_pin( BeagleBone::Pins->new($args{dc_pin} || 'P9_15') );
+    $self->rst_pin( BeagleBone::Pins->new($args{rst_pin} || 'P9_17') );
     $self->init_display;
     return $self;
 }
+
+=head2 init_display
+
+Initialises the display.
+
+Automatically called by new().
+
+=cut
 
 sub init_display {
     my $self = shift;
 
     # Using very paranoid delays here!
-    $rst->digitalWrite(1);
+    $self->rst_pin->digitalWrite(1);
     usleep(1000);
-    $rst->digitalWrite(0);
+    $self->rst_pin->digitalWrite(0);
     usleep(10000);
-    $rst->digitalWrite(1);
+    $self->rst_pin->digitalWrite(1);
     usleep(10000);
 
 
@@ -81,9 +119,18 @@ sub init_display {
     return;
 }
 
-# byte = byte to write to chip
-# mode = screen data or command
-# TODO: Convert to using SPI via kernel.. much more efficient.
+=head2 writeByte
+
+Writes a single byte to the LCD controller. Can be either 'data' or 'cmd'
+(command).
+
+Examples:
+
+  $lcd->writeByte(0x255, 'data'); # write 8 solid pixels
+  $lcd->writeByte(0xAF, 'cmd');   # Turns on screen if it was off..
+
+=cut
+
 sub writeByte {
     my ($self, $byte, $mode) = @_;
     $mode //= 'data';
@@ -91,33 +138,64 @@ sub writeByte {
     # This can't be efficient :(
     my @bits = split('', unpack('B8', pack('C', $byte)));
 
-    $dc->digitalWrite($mode eq 'cmd' ? 0 : 1 ); 
+    $self->dc_pin->digitalWrite($mode eq 'cmd' ? 0 : 1 ); 
 
     BeagleBone::SPI->SpiWrite([$byte]);
 
     # Done!
 }
 
-# Method to write an entire buffer of data to the controller
-# Assumes it's all data, no commands.
-# $bytes should be an array-reference
+=head2 writeBulk
+
+Method to write an entire buffer of data to the controller
+
+Assumes it's all data, no commands.
+
+$bytes should be an array-reference
+
+Example:
+
+  # Fill screen with static:
+  my @buf = map { int(rand(255)) } (0..511);
+  $lcd->writeBulk(\@buf);
+
+=cut
+
 sub writeBulk {
     my ($self, $bytes) = @_;
     croak "\$bytes param should be array-ref"
         unless (ref $bytes eq 'ARRAY');
 
-    $dc->digitalWrite(1); # data, not command
+    $self->dc_pin->digitalWrite(1); # data, not command
     BeagleBone::SPI->SpiWrite($bytes);
 }
+
+=head2 display_normal
+
+Sets display to normal mode -- opposite of inverse.
+
+=cut
 
 sub display_normal {
     my $self = shift;
     $self->writeByte(0xA6, 'cmd');
 }
 
+=head2 display_inverse
+
+Switch to inverse display mode. (ie. Black on white)
+
+=cut
+
 sub display_inverse {
     my $self = shift;
     $self->writeByte(0xA7, 'cmd');
 }
+
+=head1 AUTHOR
+
+Toby Corkindale (tjc @ wintrmute dot net)
+
+=cut
 
 1;
